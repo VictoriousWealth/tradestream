8b2891ba4af1eb7ffb0220209622161be7be8037
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-01-05 11:08:07 +0000

    refactor(build): removed build files from the index

--- services/orders-service/bin/main/db/migration/V5__drop_ingested_trades.sql
+++
@@ -1,3 +0,0 @@
--- Remove legacy idempotency table no longer in use
-DROP TABLE IF EXISTS ingested_trades;
--- (Dropping the table also drops idx_ingested_trades_order automatically)
--- services/orders-service/bin/main/db/migration/V6__normalize_time_in_force.sql
+++
@@ -1,1 +0,0 @@
-UPDATE orders SET time_in_force = 'GTC' WHERE time_in_force = 'DAY';
